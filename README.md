2a245d1d726cc9f3397c890a7c5fe96c3664fd9e
Brian Richard <brianrichard28238@gmail.com>
2025-05-20 20:07:49 -0300

    pratica01

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Tutorial
+# Tutorial  
 Essa é minha primeira anotação  
 terceira linha  
---
+++ pratica01.py
@@ -0,0 +1,30 @@
+# 1- Imprimir na tela a mensagem Olá mundo
+print("Olá Mundo")
+
+# 2- Realizar a soma de dois números
+print("Soma de variveis")
+numero1 = 8
+numero2 = 16
+
+soma = numero1 + numero2
+
+print(f"A soma de {numero1} e {numero2} é: {soma}")
+
+# Calculadora de Volume
+
+comprimento = 100
+largura = 100
+altura = 100
+
+volume = comprimento * altura * largura
+
+print(f"O volume da caixa é: {volume} cm³")
+
+# Calculadora de preço
+
+nome_produto = "Bolsa"
+preco_unitario = 100
+quantidade = 2
+preco_total = preco_unitario * quantidade
+
+print(f"O produto {nome_produto} tem o preço R$ {preco_unitario} reais e o valor da compra é: R$ {preco_total} reais")
